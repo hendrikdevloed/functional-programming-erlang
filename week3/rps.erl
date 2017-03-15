df4609bcf049e204218c3a2199b424ffe542b5bb
Hendrik De Vloed <Hendrik De Vloed>
2017-03-15 23:38:59 +0100

    Replaced by 3.12 exercise template

--- week3/rps.erl
+++ week3/rps.erl
@@ -1,31 +1,135 @@
 -module(rps).
--export([result/2]).
+-export([play/1,echo/1,play_two/3,rock/1,no_repeat/1,const/1,enum/1,cycle/1,rand/1,val/1,tournament/2]).
 
-beat(rock) -> paper;
-beat(paper) -> scissors;
-beat(scissors) -> rock.
 
-lose(rock) -> scissors;
-lose(scissors) -> paper;
-lose(paper) -> rock.
+%
+% play one strategy against another, for N moves.
+%
 
-result(Move,Counter) ->
-    case {beat(Move), lose(Move)} of
-        {Counter,_} -> lose;
-        {_,Counter} -> win;
-        _ -> draw
+play_two(StrategyL,StrategyR,N) ->
+    play_two(StrategyL,StrategyR,[],[],N).
+
+% tail recursive loop for play_two/3
+% 0 case computes the result of the tournament
+
+% FOR YOU TO DEFINE
+% REPLACE THE dummy DEFINITIONS
+
+play_two(_,_,PlaysL,PlaysR,0) ->
+   dummy;
+
+play_two(StrategyL,StrategyR,PlaysL,PlaysR,N) ->
+   dummy.
+
+%
+% interactively play against a strategy, provided as argument.
+%
+
+play(Strategy) ->
+    io:format("Rock - paper - scissors~n"),
+    io:format("Play one of rock, paper, scissors, ...~n"),
+    io:format("... r, p, s, stop, followed by '.'~n"),
+    play(Strategy,[]).
+
+% tail recursive loop for play/1
+
+play(Strategy,Moves) ->
+    {ok,P} = io:read("Play: "),
+    Play = expand(P),
+    case Play of
+	stop ->
+	    io:format("Stopped~n");
+	_    ->
+	    Result = result(Play,Strategy(Moves)),
+	    io:format("Result: ~p~n",[Result]),
+	    play(Strategy,[Play|Moves])
     end.
 
-% A tournament is a series of rounds – each round is a single choice from the two players,
-% which we’ll call left and right. Suppose that the choices are given as two lists; give the
-% tournament result as an integer, so that the number counts the difference between the number
-% of wins for left and right. A positive value is an overall win for left, a negative for right,
-% and zero represents an overall draw. For instance:
-% tournament([rock,rock,paper,paper],[rock,paper,scissors,rock] = -1
-% Which higher-order functions from the lists module can you use in computing this solution?
+%
+% auxiliary functions
+%
 
-% => 
-%    lists:zip to join individual moves from both lists into a pair of moves for a single game
-%    lists:map to evaluate the result of each game individually
-%    lists:map to convert a win/lose/draw result into a numeric score
-%    lists:foldr to coalesce all individual scores into a final score,
+% transform shorthand atoms to expanded form
+    
+expand(r) -> rock;
+expand(p) -> paper;		    
+expand(s) -> scissors;
+expand(X) -> X.
+
+% result of one set of plays
+
+result(rock,rock) -> draw;
+result(rock,paper) -> lose;
+result(rock,scissors) -> win;
+result(paper,rock) -> win;
+result(paper,paper) -> draw;
+result(paper,scissors) -> lose;
+result(scissors,rock) -> lose;
+result(scissors,paper) -> win;
+result(scissors,scissors) -> draw.
+
+% result of a tournament
+
+tournament(PlaysL,PlaysR) ->
+    lists:sum(
+      lists:map(fun outcome/1,
+		lists:zipwith(fun result/2,PlaysL,PlaysR))).
+
+outcome(win)  ->  1;
+outcome(lose) -> -1;
+outcome(draw) ->  0.
+
+% transform 0, 1, 2 to rock, paper, scissors and vice versa.
+
+enum(0) ->
+    rock;
+enum(1) ->
+    paper;
+enum(2) ->
+    scissors.
+
+val(rock) ->
+    0;
+val(paper) ->
+    1;
+val(scissors) ->
+    2.
+
+% give the play which the argument beats.
+
+beats(rock) ->
+    scissors;
+beats(paper) ->
+    rock;
+beats(scissors) ->
+    paper.
+
+%
+% strategies.
+%
+echo([]) ->
+     paper;
+echo([Last|_]) ->
+    Last.
+
+rock(_) ->
+    rock.
+
+
+
+% FOR YOU TO DEFINE
+% REPLACE THE dummy DEFINITIONS
+
+no_repeat([]) ->
+    dummy;
+no_repeat([X|_]) ->
+    dummy.
+
+const(Play) ->
+    dummy.
+
+cycle(Xs) ->
+    dummy.
+
+rand(_) ->
+    dummy.
